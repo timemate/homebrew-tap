488bbb456389ffa8bc42844d86f8da05ea67dcc9
Andrew Red <andrew@reddikh.com>
2021-10-13 23:56:12 +0300

    - added plist

--- Formula/toggl-sync.rb
+++ Formula/toggl-sync.rb
@@ -19,4 +19,37 @@ class TogglSync < Formula
   test do
     assert_match /Tiny service to sync time entries from toggl to jira/, shell_output("#{bin}/toggl-sync -h", 0)
   end
+
+  ## Plist handling
+  # Does your plist need to be loaded at startup?
+  plist_options :startup => true
+  # Or only when necessary or desired by the user?
+  plist_options :manual => "toggl-sync"
+  # Or perhaps you'd like to give the user a choice? Ooh fancy.
+  plist_options :startup => "true", :manual => "toggl-sync --service"
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+        <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{bin}/toggl-sync</string>
+        <string>-period 1w</string>
+        <string>--service</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>KeepAlive</key>
+      <true/>
+      <key>StandardErrorPath</key>
+      <string>/dev/null</string>
+      <key>StandardOutPath</key>
+      <string>/dev/null</string>
+    </plist>
+    EOS
+  end
 end
